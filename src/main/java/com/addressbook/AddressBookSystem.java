5fbb560853ae3ea8d0579e871e2411eb2b3a18eb
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-05-12 09:24:56 +0530

    Ability To Count Person Using Streams

--- src/main/java/com/addressbook/AddressBookSystem.java
+++ src/main/java/com/addressbook/AddressBookSystem.java
@@ -1,7 +1,6 @@
 package com.addressbook;
 import java.util.*;
 public class AddressBookSystem {
-
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
 
@@ -16,7 +15,8 @@ public class AddressBookSystem {
             System.out.println("5. Show address book system");
             System.out.println("6. Search person by the city or sate");
             System.out.println("7. View person by the city or state");
-            System.out.println("8. Exit");
+            System.out.println("8. Count by city or sate");
+            System.out.println("9. Exit");
 
             int option=sc.nextInt();
 
@@ -126,9 +126,26 @@ public class AddressBookSystem {
             }
 
             if(option==8) {
+                System.out.println("Enter the state/city name to count the persons:");
+                sc.nextLine();
+                String cityOrStateName = sc.nextLine();
+                List<String> personsInCityOrState = addressBookMain.searchPersonByCityorState(cityOrStateName);
+                if(personsInCityOrState.size()==0)
+                    System.out.println("Sorry, there is no person in the "+cityOrStateName+".");
+                else {
+                    addressBookMain.countPersonByCityorState(cityOrStateName);
+                }
+                continue;
+            }
+
+            if(option==9) {
                 System.out.println("Thank you.");
                 break;
             }
+
         }
+
+
     }
+
 }
